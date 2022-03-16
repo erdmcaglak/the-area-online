fa8b19457e429225315254a6e94cf5323ab08784
erdmcaglak <erdem.caglak@gmail.com>
2022-03-17 00:29:01 +0300

    'first'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <DetailTakeTime/> -->
+  </div>
+</template>
+
+<script>
+import DetailTakeTime from "@/components/DetailTakeTime.vue"
+export default {
+  components:{
+    DetailTakeTime
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/DetailTakeTime.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="detail-main">
+    <div class="coming-soon">
+      Details take time.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<style lang="scss">
+.detail-main{
+  height: 100%;
+  background: url('../assets/images/coming_soon.jpg');
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  .coming-soon{
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 96px;
+    width: 100%;
+    height: 100%;
+    color: #fff;
+    font-weight: 600;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    user-select: none;
+  }
+}
+*{
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  text-decoration: none;
+  box-sizing: border-box;
+}
+html,body{
+  height: 100%;
+}
+</style>
